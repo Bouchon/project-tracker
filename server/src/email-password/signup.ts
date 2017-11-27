f8fc8d883e341c8b3cc9bbb36ab4d28fe2ac9c54
Florian Knoblauch <florian.knoblauch@bihr.eu>
2017-11-27 18:37:11 +0100

    login

--- server/src/email-password/signup.ts
+++ server/src/email-password/signup.ts
@@ -8,6 +8,7 @@ interface User {
 }
 
 interface EventData {
+  name: string
   email: string
   password: string
 }
@@ -21,7 +22,11 @@ export default async (event: FunctionEvent<EventData>) => {
     const graphcool = fromEvent(event)
     const api = graphcool.api('simple/v1')
 
-    const { email, password } = event.data
+    const { name, email, password } = event.data
+
+    if (validator.isEmpty(name)) {
+      return { error: 'Not a valid name' }
+    }
 
     if (!validator.isEmail(email)) {
       return { error: 'Not a valid email' }
@@ -39,7 +44,7 @@ export default async (event: FunctionEvent<EventData>) => {
     const hash = await bcrypt.hash(password, SALT_ROUNDS)
 
     // create new user
-    const userId = await createGraphcoolUser(api, email, hash)
+    const userId = await createGraphcoolUser(api, name, email, hash)
 
     // generate node token for new User node
     const token = await graphcool.generateNodeToken(userId, 'User')
@@ -67,10 +72,11 @@ async function getUser(api: GraphQLClient, email: string): Promise<{ User }> {
   return api.request<{ User }>(query, variables)
 }
 
-async function createGraphcoolUser(api: GraphQLClient, email: string, password: string): Promise<string> {
+async function createGraphcoolUser(api: GraphQLClient, name: string, email: string, password: string): Promise<string> {
   const mutation = `
-    mutation createGraphcoolUser($email: String!, $password: String!) {
+    mutation createGraphcoolUser($name: String!, $email: String!, $password: String!) {
       createUser(
+        name: $name,
         email: $email,
         password: $password
       ) {
@@ -80,6 +86,7 @@ async function createGraphcoolUser(api: GraphQLClient, email: string, password: 
   `
 
   const variables = {
+    name,
     email,
     password
   }
--- src/components/App/Login.js
+++ src/components/App/Login.js
@@ -22,48 +22,85 @@ const css = {
     }
 }
 class Login extends Component {
-    state = { email: '', password: '', isLoading: false }
+    state = { isLogin: true, email: '', name: '', password: '', isLoginLoading: false, isSignInLoading: false }
     
     clickLogin = async (event) => {
         const { email, password } = this.state
-        const { onRequestClose, onLoginResult } = this.props
-        this.setState({ isLoading: true })
+        const { onLoginResult } = this.props
+        this.setState({ isLoginLoading: true })
         
         this.props.authenticateUserMutation({ variables: { email, password } })
-        .then(({ data }) => {
-            this.setState({ isLoading: false })
-            onLoginResult({ id: data.authenticateUser.id, email, token: data.authenticateUser.token })
-        }) 
-        .catch(error => {
-            this.setState({ isLoading: false })
-            onLoginResult({ error: error.graphQLErrors[0].functionError })
-        })
+            .then(({ data }) => {
+                this.setState({ isLoading: false })
+                onLoginResult({ id: data.authenticateUser.id, email, token: data.authenticateUser.token })
+            }) 
+            .catch(error => {
+                this.setState({ isLoading: false })
+                onLoginResult({ error: error.graphQLErrors[0].functionError })
+            })
+    }
+
+    clickSignin = async (event) => {
+        const { name, email, password } = this.state
+        const { onSignInResult } = this.props
+        this.setState({ isSignInLoading: true })
+
+        const signinResult = this.props.signupUserMutation({ variables: { name, email, password } })
+            .then(({ data }) => {
+                this.setState({ isSignInLoading: false })
+                onSignInResult({ id: data.signupUser.id, email, token: data.signupUser.token })
+            })
+            .catch(error => {
+                this.setState({ isSignInLoading: false })
+                onSignInResult({ error: error.graphQLErrors[0].functionError })
+            })
     }
 
     render () {
-        const { email, password, isLoading } = this.state
+        const { isLogin, email, name, password, isLoginLoading, isSignInLoading } = this.state
+        
+        const loginBtn = (
+            <Button onClick={ () => this.clickLogin() } raised color='accent'>
+                { isLoginLoading && <CircularProgress size={ 20 } color='accent' style={{ marginRight: '1em' }} /> }
+                Login
+            </Button>
+        )
+
+        const signInBtn = (
+            <Button onClick={ () => this.clickSignin() } raised color='accent'>
+                { isSignInLoading && <CircularProgress size={ 20 } color='accent' style={{ marginRight: '1em' }} /> }
+                Sign in
+            </Button>
+        )
+
+        const switchBtn = <Button onClick={ () => this.setState({ isLogin: !isLogin }) }>{ isLogin ? 'Create an account' : 'Already have an account ?' }</Button>
 
         return (
             <div style={ css.container }>
-                <Typography type='display1'>Login</Typography>
+                <Typography type='display1'>Connection</Typography>
                 <br />
                 <TextField style={ css.inputs } label='Email' type='email' value={ email } onChange={ evt => this.setState({ email: evt.target.value }) } />
+                <br />
+                { !isLogin && <TextField style={ css.inputs } label='Name' type='text' value={ name } onChange={ evt => this.setState({ name: evt.target.value }) } /> }
+                { !isLogin && <br /> }
                 <TextField style={ css.inputs } label='Password' type='password' value={ password } onChange={ evt => this.setState({ password: evt.target.value }) } />
                 <br />
-                <Button onClick={ () => this.clickLogin() } raised color='accent'>
-                    { isLoading && <CircularProgress size={ 20 } color='accent' style={{ marginRight: '1em' }} /> }
+                { isLogin ? loginBtn : signInBtn }
+                {/* <Button onClick={ () => this.clickLogin() } raised color='accent'>
+                    { isLoginLoading && <CircularProgress size={ 20 } color='accent' style={{ marginRight: '1em' }} /> }
                     Login
-                </Button>
+                </Button> */}
                 <br />
-                <Button>Create an account</Button>
+                { switchBtn }
             </div>
         )
     }
 }
 
 const SIGNUP_USER_MUTATION = gql`
-  mutation SignupUserMutation($email: String!, $password: String!) {
+  mutation SignupUserMutation($name: String!, $email: String!, $password: String!) {
     signupUser(
+      name: $name,
       email: $email,
       password: $password
     ) {
